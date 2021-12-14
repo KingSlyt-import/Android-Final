4392e012b5b0ca3341920602ce3c7d7e65aad190
Nauht <vthuan26655@gmail.com>
2021-12-14 22:04:54 +0700

    skeleton 2.0

--- app/src/main/java/com/example/android_final/CreateCategories.java
+++ app/src/main/java/com/example/android_final/CreateCategories.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CreateCategories extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
