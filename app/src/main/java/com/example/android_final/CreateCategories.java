4affbd3ebf90cde4930c3486a096bf52d47ac7f6
Nauht <vthuan26655@gmail.com>
2021-12-16 01:35:56 +0700

    adjust xml a little - test spinner for choosing color n icon

--- app/src/main/java/com/example/android_final/CreateCategories.java
+++ app/src/main/java/com/example/android_final/CreateCategories.java
@@ -1,13 +1,46 @@
 package com.example.android_final;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CreateCategories extends AppCompatActivity {
+
+    TextView cate_icon;
+
+    List<String> color = Arrays.asList("Black", "Pink!", "Blue", "Red", "Brown");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_categories);
+//        cate_icon = findViewById(R.id.cate_icon);
+//
+//        cate_icon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+        Spinner spinner = (Spinner) findViewById(R.id.color_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.color_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
 }
