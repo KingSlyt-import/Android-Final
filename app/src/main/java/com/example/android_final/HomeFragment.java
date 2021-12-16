154bed81c45d4979ede6eb42efa1f9c62b7f2a55
Nauht <vthuan26655@gmail.com>
2021-12-16 22:53:44 +0700

    another minor update

--- app/src/main/java/com/example/android_final/HomeFragment.java
+++ app/src/main/java/com/example/android_final/HomeFragment.java
@@ -1,12 +1,20 @@
 package com.example.android_final;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +32,9 @@ public class HomeFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private TextView home_day;
+    private TextView home_month;
+    private TextView home_year;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -55,10 +66,22 @@ public class HomeFragment extends Fragment {
         }
     }
 
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_home_page, container, false);
+        View view = inflater.inflate(R.layout.activity_home_page, container, false);
+        String dayOfMonth = LocalDateTime.now().getDayOfMonth()+"";
+        String month = LocalDateTime.now().getMonth().name()+"";
+        String year = LocalDateTime.now().getYear()+"";
+        home_day = view.findViewById(R.id.home_day);
+        home_month = view.findViewById(R.id.home_month);
+        home_year = view.findViewById(R.id.home_year);
+        home_day.setText(dayOfMonth);
+        home_month.setText(month);
+        home_year.setText(year);
+        return view;
     }
 }
