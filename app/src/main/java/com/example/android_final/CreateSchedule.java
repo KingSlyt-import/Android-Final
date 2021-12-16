009eaa4460dbf0af5d7e43bb37dbcf4958c02675
Nauht <vthuan26655@gmail.com>
2021-12-16 15:51:05 +0700

    change task, sche, cate a little

--- app/src/main/java/com/example/android_final/CreateSchedule.java
+++ app/src/main/java/com/example/android_final/CreateSchedule.java
@@ -3,12 +3,17 @@ package com.example.android_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TimePicker;
 
 public class CreateSchedule extends AppCompatActivity {
 
+    TimePicker timePicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_schedule);
+
+        timePicker = findViewById(R.id.timePicker);
+        timePicker.setIs24HourView(false);
     }
 }
---
+++ app/src/main/java/com/example/android_final/ImageSpinnerAdapter.java
@@ -0,0 +1,45 @@
+package com.example.android_final;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+class ImageSpinnerAdapter extends ArrayAdapter<Integer> {
+    private Integer[] images;
+    private Context ctx;
+
+    public ImageSpinnerAdapter(Context context, Integer[] images) {
+        super(context, android.R.layout.simple_spinner_item, images);
+        this.images = images;
+        this.ctx = context;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        return getImageForPosition(position);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.icon_spinner_layout, null);
+        }
+
+        ImageView imageView = (ImageView)convertView.findViewById(R.id.spinnerImages);
+        imageView.setImageResource(images[position]);
+        return convertView;
+    }
+
+    private View getImageForPosition(int position) {
+        ImageView imageView = new ImageView(getContext());
+        imageView.setBackgroundResource(images[position]);
+        imageView.setLayoutParams(new AbsListView.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        return imageView;
+    }
+}
