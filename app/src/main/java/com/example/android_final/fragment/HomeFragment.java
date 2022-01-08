f9b37c10b96c3a6f27284b655581bbda843b6067
Nauht <vthuan26655@gmail.com>
2022-01-08 21:50:41 +0700

    homefragment recyclerview

--- app/src/main/java/com/example/android_final/fragment/HomeFragment.java
+++ app/src/main/java/com/example/android_final/fragment/HomeFragment.java
@@ -5,15 +5,28 @@ import android.os.Bundle;
 
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.android_final.R;
+import com.example.android_final.adapter.TaskAdapter;
+import com.example.android_final.data.Task;
+import com.google.firebase.database.annotations.Nullable;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,14 +39,17 @@ public class HomeFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "test";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    FirebaseFirestore db;
 
     private TextView home_day;
     private TextView home_month;
     private TextView home_year;
+    private TextView display_tasks;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -81,6 +97,39 @@ public class HomeFragment extends Fragment {
         home_day.setText(dayOfMonth);
         home_month.setText(month);
         home_year.setText(year);
+        display_tasks = view.findViewById(R.id.display_tasks);
+
+        List<Task> taskList = new ArrayList<>();
+        RecyclerView task_recyclerview = view.findViewById(R.id.home_task_recyclerview);
+        task_recyclerview.setHasFixedSize(true);
+        TaskAdapter taskAdapter = new TaskAdapter(getActivity(), taskList);
+        task_recyclerview.setAdapter(taskAdapter);
+        task_recyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
+        //db
+        db = FirebaseFirestore.getInstance();
+        int thismonth = LocalDateTime.now().getMonthValue();
+        String time = dayOfMonth+(thismonth < 10 ? "/0"+LocalDateTime.now().getMonthValue() : "/"+LocalDateTime.now().getMonth()) + "/" + LocalDateTime.now().getYear();
+        Log.d(TAG, "onCreateView: " +time);
+        db.collection("tasks")
+                .whereEqualTo("Day", time)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value,
+                                        @Nullable FirebaseFirestoreException e) {
+                        taskList.clear();
+                        taskAdapter.notifyDataSetChanged();
+                        if (e != null) {
+                            return;
+                        }
+                        for (QueryDocumentSnapshot doc : value) {
+                            if (doc.get("Name")!=null) {
+                                taskList.add(new Task(doc.getString("Name"), doc.getString("Importance"), doc.getString("Day"), doc.getString("Note"), doc.getId(), false));
+                                taskAdapter.notifyDataSetChanged();
+                            }
+                        }
+                        display_tasks.setText("You have "+((taskList.size()>1) ? taskList.size()+" tasks today": taskList.size()+" task today"));
+                    }
+                });
         return view;
     }
 }
--- app/src/main/java/com/example/android_final/fragment/ListFragment.java
+++ app/src/main/java/com/example/android_final/fragment/ListFragment.java
@@ -80,34 +80,34 @@ public class ListFragment extends Fragment {
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_list, container, false);
-        List<Bubble> messagesList = new ArrayList<>();
-        RecyclerView bubble_schedule = view.findViewById(R.id.bubble_schedule);
-        bubble_schedule.setHasFixedSize(true);
-        BubbleAdapter bubbleAdapter = new BubbleAdapter(getActivity(), messagesList);
-        bubble_schedule.setAdapter(bubbleAdapter);
-        bubble_schedule.setLayoutManager(new GridLayoutManager(getActivity(), 3));
-
-        db = FirebaseFirestore.getInstance();
-        db.collection("schedules")
-                .addSnapshotListener(new EventListener<QuerySnapshot>() {
-                    @Override
-                    public void onEvent(@Nullable QuerySnapshot value,
-                                        @Nullable FirebaseFirestoreException e) {
-                        if (e != null) {
-
-                            return;
-                        }
-                        messagesList.clear();
-                        for (QueryDocumentSnapshot doc : value) {
-                            if (doc.get("Name")!=null && doc.get("Remain")!=null) {
-                                messagesList.add(new Bubble(R.drawable.icon1, doc.getString("Name"), doc.getString("Remain"), doc.getId()));
-                                bubbleAdapter.notifyDataSetChanged();
-                            }
-
-                        }
-                    }
-
-                });
+//        List<Bubble> messagesList = new ArrayList<>();
+//        RecyclerView bubble_schedule = view.findViewById(R.id.bubble_schedule);
+//        bubble_schedule.setHasFixedSize(true);
+//        BubbleAdapter bubbleAdapter = new BubbleAdapter(getActivity(), messagesList);
+//        bubble_schedule.setAdapter(bubbleAdapter);
+//        bubble_schedule.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+//
+//        db = FirebaseFirestore.getInstance();
+//        db.collection("schedules")
+//                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onEvent(@Nullable QuerySnapshot value,
+//                                        @Nullable FirebaseFirestoreException e) {
+//                        if (e != null) {
+//
+//                            return;
+//                        }
+//                        messagesList.clear();
+//                        for (QueryDocumentSnapshot doc : value) {
+//                            if (doc.get("Name")!=null && doc.get("Remain")!=null) {
+//                                messagesList.add(new Bubble(R.drawable.icon1, doc.getString("Name"), doc.getString("Remain"), doc.getId()));
+//                                bubbleAdapter.notifyDataSetChanged();
+//                            }
+//
+//                        }
+//                    }
+//
+//                });
         return view;
     }
 }
